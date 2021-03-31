79ef32f5a893fb47afdb13ee552b93b7466617b5
Rachel Solomon <37978186+solomor1@users.noreply.github.com>
2021-03-31 19:08:26 +0100

    Add files via upload

---
+++ src/flask_app/mytests.py
@@ -0,0 +1,19 @@
+import unittest
+from helloworld import *
+
+class MyFirstTests(unittest.TestCase):
+
+    def test_hello(self):
+        self.assertEqual(hello_world(), 'hello world')
+
+    def test_custom_num_list(self):
+        self.assertEqual(len(create_num_list(10)),10)
+
+    def test_custom_func_x(self):
+        self.assertEqual(custom_func_x(3,2,3), 54)
+
+    def test_addition(self):
+        self.assertEqual(customAdd(2,2), 4)
+
+if __name__ == '__main__':
+    unittest.main()
